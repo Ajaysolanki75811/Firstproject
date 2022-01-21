ce4ec4856c38b47cc68f659dfb238999170f49c2
Ajaysolanki75811 <ajaysolanki75811@gmail.com>
2022-01-21 23:53:29 +0530

    main method added

--- Intro.java
+++ Intro.java
@@ -1,5 +1,13 @@
-public class Intro {
-    public static void main(String[] args) {
+import jdk.nashorn.internal.ir.Node;
 
+public class Intro {
+    private static Node greet;
+
+    public static void main(String[] args) {
+        String a = "Hello World";
+        System.out.println("String is" + a);
+
+        int length = greet.length();
+        System.out.println("length is "+ length);
     }
 }
