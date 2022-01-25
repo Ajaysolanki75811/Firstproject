8410c8473e1d170af5c9c004b1eaae0de91d5c9e
Ajaysolanki75811 <ajaysolanki75811@gmail.com>
2022-01-25 17:45:02 +0530

    main method added

--- Intro.java
+++ Intro.java
@@ -4,9 +4,13 @@ public class Intro {
 
     public static void main(String[] args) {
         String a = "Hello World abcd" ;
-        System.out.println("String is " + a);
-
+        System.out.println("String is = " + a);
         int i = a.length();
-        System.out.println("length is "+ i);
+        System.out.println("length is ="+ i);
+
+        String b = "Wel Come To GitHub";
+        int c = b.length();
+        System.out.println("String b is =" + b);
+        System.out.println("Length of b is ="  + c);
     }
 }
